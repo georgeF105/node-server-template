324b6b726f0bb79c5514537d32b5798cb83d315c
George Francis <georgefrancis105@gmail.com>
2016-06-18 13:34:19 +1200

    Setup handlebars homepage

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var app = express()
+var path = require('path')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('homePage',{message:"homepage test message"})
 })
 
 app.listen(3000, function () {
